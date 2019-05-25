806f05728182598a5693384b49e18d7b577caeee
Gabriel <34012267+HyperPolygon64@users.noreply.github.com>
2019-05-25 14:59:18 +0100

    Add Visual Studio project to repository

---
+++ SP2 Tool/Main.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace SP2_Tool
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.menu_Main = new System.Windows.Forms.MenuStrip();
+            this.main_File = new System.Windows.Forms.ToolStripMenuItem();
+            this.file_ExportSP2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.file_Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.ofd_OpenFiles = new System.Windows.Forms.OpenFileDialog();
+            this.file_OpenFolder = new System.Windows.Forms.ToolStripMenuItem();
+            this.fbd_OpenFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.web_Explorer = new System.Windows.Forms.WebBrowser();
+            this.main_SDK = new System.Windows.Forms.ToolStripMenuItem();
+            this.sdk_CreateSP2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.main_Help = new System.Windows.Forms.ToolStripMenuItem();
+            this.main_About = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.main_GitHub = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_Main.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu_Main
+            // 
+            this.menu_Main.BackColor = System.Drawing.SystemColors.Control;
+            this.menu_Main.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.main_File,
+            this.main_SDK,
+            this.main_Help});
+            this.menu_Main.Location = new System.Drawing.Point(0, 0);
+            this.menu_Main.Name = "menu_Main";
+            this.menu_Main.Size = new System.Drawing.Size(642, 24);
+            this.menu_Main.TabIndex = 0;
+            this.menu_Main.Text = "menuStrip1";
+            // 
+            // main_File
+            // 
+            this.main_File.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.file_ExportSP2,
+            this.file_OpenFolder,
+            this.toolStripSeparator1,
+            this.file_Exit});
+            this.main_File.Name = "main_File";
+            this.main_File.Size = new System.Drawing.Size(37, 20);
+            this.main_File.Text = "File";
+            // 
+            // file_ExportSP2
+            // 
+            this.file_ExportSP2.Image = ((System.Drawing.Image)(resources.GetObject("file_ExportSP2.Image")));
+            this.file_ExportSP2.Name = "file_ExportSP2";
+            this.file_ExportSP2.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.file_ExportSP2.Size = new System.Drawing.Size(205, 22);
+            this.file_ExportSP2.Text = "Open SP2";
+            this.file_ExportSP2.Click += new System.EventHandler(this.File_OpenSP2_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(202, 6);
+            // 
+            // file_Exit
+            // 
+            this.file_Exit.Image = ((System.Drawing.Image)(resources.GetObject("file_Exit.Image")));
+            this.file_Exit.Name = "file_Exit";
+            this.file_Exit.Size = new System.Drawing.Size(205, 22);
+            this.file_Exit.Text = "Exit";
+            this.file_Exit.Click += new System.EventHandler(this.File_Exit_Click);
+            // 
+            // file_OpenFolder
+            // 
+            this.file_OpenFolder.Image = ((System.Drawing.Image)(resources.GetObject("file_OpenFolder.Image")));
+            this.file_OpenFolder.Name = "file_OpenFolder";
+            this.file_OpenFolder.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Alt) 
+            | System.Windows.Forms.Keys.O)));
+            this.file_OpenFolder.Size = new System.Drawing.Size(205, 22);
+            this.file_OpenFolder.Text = "Open Folder";
+            this.file_OpenFolder.Visible = false;
+            this.file_OpenFolder.Click += new System.EventHandler(this.File_OpenFolder_Click);
+            // 
+            // web_Explorer
+            // 
+            this.web_Explorer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.web_Explorer.Location = new System.Drawing.Point(0, 24);
+            this.web_Explorer.MinimumSize = new System.Drawing.Size(20, 20);
+            this.web_Explorer.Name = "web_Explorer";
+            this.web_Explorer.Size = new System.Drawing.Size(642, 387);
+            this.web_Explorer.TabIndex = 1;
+            // 
+            // main_SDK
+            // 
+            this.main_SDK.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sdk_CreateSP2});
+            this.main_SDK.Name = "main_SDK";
+            this.main_SDK.Size = new System.Drawing.Size(40, 20);
+            this.main_SDK.Text = "SDK";
+            // 
+            // sdk_CreateSP2
+            // 
+            this.sdk_CreateSP2.Enabled = false;
+            this.sdk_CreateSP2.Image = ((System.Drawing.Image)(resources.GetObject("sdk_CreateSP2.Image")));
+            this.sdk_CreateSP2.Name = "sdk_CreateSP2";
+            this.sdk_CreateSP2.Size = new System.Drawing.Size(180, 22);
+            this.sdk_CreateSP2.Text = "Create SP2";
+            this.sdk_CreateSP2.Click += new System.EventHandler(this.CreateSP2ToolStripMenuItem_Click);
+            // 
+            // main_Help
+            // 
+            this.main_Help.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.main_GitHub,
+            this.toolStripSeparator2,
+            this.main_About});
+            this.main_Help.Name = "main_Help";
+            this.main_Help.Size = new System.Drawing.Size(44, 20);
+            this.main_Help.Text = "Help";
+            // 
+            // main_About
+            // 
+            this.main_About.Image = ((System.Drawing.Image)(resources.GetObject("main_About.Image")));
+            this.main_About.Name = "main_About";
+            this.main_About.Size = new System.Drawing.Size(180, 22);
+            this.main_About.Text = "About";
+            this.main_About.Click += new System.EventHandler(this.Main_About_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // main_GitHub
+            // 
+            this.main_GitHub.Image = ((System.Drawing.Image)(resources.GetObject("main_GitHub.Image")));
+            this.main_GitHub.Name = "main_GitHub";
+            this.main_GitHub.Size = new System.Drawing.Size(180, 22);
+            this.main_GitHub.Text = "GitHub...";
+            this.main_GitHub.Click += new System.EventHandler(this.Main_GitHub_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 411);
+            this.Controls.Add(this.web_Explorer);
+            this.Controls.Add(this.menu_Main);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menu_Main;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SP2 Tool";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.menu_Main.ResumeLayout(false);
+            this.menu_Main.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menu_Main;
+        private System.Windows.Forms.ToolStripMenuItem main_File;
+        private System.Windows.Forms.ToolStripMenuItem file_ExportSP2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem file_Exit;
+        private System.Windows.Forms.OpenFileDialog ofd_OpenFiles;
+        private System.Windows.Forms.ToolStripMenuItem file_OpenFolder;
+        private System.Windows.Forms.FolderBrowserDialog fbd_OpenFolder;
+        private System.Windows.Forms.WebBrowser web_Explorer;
+        private System.Windows.Forms.ToolStripMenuItem main_SDK;
+        private System.Windows.Forms.ToolStripMenuItem sdk_CreateSP2;
+        private System.Windows.Forms.ToolStripMenuItem main_Help;
+        private System.Windows.Forms.ToolStripMenuItem main_About;
+        private System.Windows.Forms.ToolStripMenuItem main_GitHub;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+    }
+}
+
---
+++ SP2 Tool/Main.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace SP2_Tool
+{
+    public partial class Main : Form
+    {
+        public Main(string[] args)
+        {
+            InitializeComponent();
+        }
+
+        #region Session ID...
+        public static Random generateSessionID = new Random();
+        public static int sessionID = generateSessionID.Next(1, 99999); //Generates a random number between 1 to 99999 for a unique Session ID.
+        #endregion
+
+        readonly string versionNumber = "Version 0.1-alpha";
+
+        void File_OpenSP2_Click(object sender, EventArgs e)
+        {
+            ofd_OpenFiles.Title = "Please select a SP2 file...";
+            ofd_OpenFiles.Filter = "SP2 Files|*.sp2";
+            ofd_OpenFiles.DefaultExt = "sp2";
+
+            if (ofd_OpenFiles.ShowDialog() == DialogResult.OK)
+            {
+                ParseSP2();
+            }
+        }
+
+        void ParseSP2()
+        {
+            try
+            {
+                string archivesPath = applicationData + @"\Hyper_Development_Team\SP2 Tool\Archives\";
+                string failsafeCheck = Path.GetRandomFileName();
+                string finalPath = archivesPath + sessionID + @"\" + failsafeCheck + @"\" + Path.GetFileNameWithoutExtension(ofd_OpenFiles.FileName) + ".sp2";
+
+                Directory.CreateDirectory(finalPath);
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+
+                using (FileStream File = new FileStream(ofd_OpenFiles.FileName, FileMode.Open))
+                {
+                    FileStream fout = null;
+                    byte[] buf = new byte[1024];
+                    int bpos = 0, blen = 0;
+
+                    int file = 0, mode = -1;
+
+                    // mode:
+                    // 0-2 'DDS '
+                    // 0, 3-4 'DXBC'
+
+                    while (true)
+                    {
+                        blen = File.Read(buf, 0, buf.Length);
+                        if (blen == 0) break;
+                        int index = 0, startpos = 0;
+
+                        for (int i = 0; i < blen; i++)
+                        {
+                            // check if we need to change mode
+
+                            switch ((char)buf[i])
+                            {
+                                case 'D':
+                                    if (mode == -1 || mode == 0)
+                                    {
+                                        if (mode == -1) startpos = i;
+                                        mode++;
+
+                                    }
+                                    else goto default;
+                                    break;
+
+                                case 'S':
+                                    if (mode == 1)
+                                        mode = 2;
+                                    else goto default;
+                                    break;
+
+                                case ' ':
+                                    if (mode == 2)
+                                    {
+                                        mode = -1;
+
+                                        // write data into disk
+                                        if (startpos > 0)
+                                        {
+                                            if (fout == null)
+                                                throw new Exception("No file was defined before data was to be written to disk! " + (File.Position - buf.Length + startpos));
+
+                                            fout.Write(buf, index, startpos - index);
+                                            fout.Close();
+                                            index = startpos;
+                                        }
+
+                                        // dis is dds file, process as dds
+                                        //  Console.WriteLine("DDS " + file + " at " + (File.Position + startpos).ToString("X"));
+                                        fout = new FileStream(finalPath + @"\" + file++ + ".dds", FileMode.Create);
+
+                                    }
+                                    else goto default;
+                                    break;
+
+                                case 'X':
+                                    if (mode == 0)
+                                        mode = 3;
+                                    else goto default;
+                                    break;
+
+                                case 'B':
+                                    if (mode == 3)
+                                        mode = 4;
+                                    else goto default;
+                                    break;
+
+                                case 'C':
+                                    if (mode == 4)
+                                    {
+                                        mode = -1;
+
+                                        // write data into disk
+                                        if (startpos > 0)
+                                        {
+                                            if (fout == null)
+                                                throw new Exception("No file was defined before data was to be written to disk! " + (File.Position - buf.Length + startpos));
+
+                                            fout.Write(buf, index, startpos - index);
+                                            fout.Close();
+                                            index = startpos;
+                                        }
+
+                                        // dis is dxbc file
+                                        //  Console.WriteLine("DXBC "+ file +" at " + (File.Position + startpos).ToString("X"));
+                                        fout = new FileStream(finalPath + @"\" + file++ + ".o", FileMode.Create);
+
+                                    }
+                                    else goto default;
+                                    break;
+
+                                default:
+                                    mode = -1;
+                                    break;
+                            }
+                        }
+
+                        if (fout == null)
+                            throw new Exception("No file was defined before data was to be written to disk! " + File.Position);
+
+                        // write data into file
+                        if (mode == -1)
+                        {
+                            fout.Write(buf, index, blen - index);
+                        }
+                        else
+                        {
+                            // uh oh, mode was not cleared, this means we read part of the header.
+                            fout.Write(buf, index, startpos - index);
+                            File.Seek(buf.Length - startpos, SeekOrigin.Current);
+                        }
+                    }
+
+                    fout?.Close();
+                }
+
+                sw.Stop();
+                //Console.WriteLine("Complete in " + sw.ElapsedMilliseconds + "ms");
+
+                sdk_CreateSP2.Enabled = true;
+                web_Explorer.Navigate(finalPath);
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex);
+                MessageBox.Show("An error occurred:\n\n" + ex, "Fatal Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        void File_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        void File_OpenFolder_Click(object sender, EventArgs e)
+        {
+            fbd_OpenFolder.Description = "Select a folder that contains DDS files.";
+
+            if (fbd_OpenFolder.ShowDialog() == DialogResult.OK)
+            {
+                web_Explorer.Navigate(fbd_OpenFolder.SelectedPath);
+            }
+        }
+
+        void CreateSP2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /********************************************/
+            /*    VERY INCOMPLETE AND UNUSABLE CODE!!   */
+            /********************************************/
+
+            MessageBox.Show("This may produce undesirable results and the resulting SP2 file may not work with Team Sonic Racing.\n\nPlease visit the GitHub page for more information.", "SP2 Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            string currentPath = web_Explorer.Url.ToString().Replace("file:///", "").Replace("/", @"\") + @"\";
+
+            if (Directory.GetFiles(currentPath, "*.dds").Length == 0) MessageBox.Show("There are no DDS files to pack in this directory.", "No DDS files available", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+            {
+                var getDDS = new StringBuilder();
+                // get all DirectDraw Surface textures
+                var allDDS = Directory.GetFiles(currentPath, "*.dds", SearchOption.TopDirectoryOnly).OrderBy(f =>
+                {
+                    int number;
+                    if (int.TryParse(Path.GetFileNameWithoutExtension(f), out number))
+                        return number;
+                    else
+                        return -1; // or some other fallback value
+                });
+                foreach (string DDS in allDDS)
+                {
+                    //Console.WriteLine("{0}", DDS);
+                    getDDS.Append("\"");
+                    getDDS.Append(Path.GetFileName(DDS));
+                    getDDS.Append("\"");
+                    getDDS.Append(" + ");
+                }
+
+                // get all DirectX Shader Bytecode
+                var allDXBC = Directory.GetFiles(currentPath, "*.o", SearchOption.TopDirectoryOnly).OrderBy(f =>
+                {
+                    int number;
+                    if (int.TryParse(Path.GetFileNameWithoutExtension(f), out number))
+                        return number;
+                    else
+                        return -1; // or some other fallback value
+                });
+                foreach (string O in allDXBC)
+                {
+                    //Console.WriteLine("{0}", O);
+                    getDDS.Append("\"");
+                    getDDS.Append(Path.GetFileName(O));
+                    getDDS.Append("\"");
+                    getDDS.Append(" + ");
+                }
+
+                var allFiles = getDDS.ToString().Remove(getDDS.ToString().Length - 3);
+                //MessageBox.Show(allFiles);
+                var createSP2 = new ProcessStartInfo("cmd.exe", "/C copy /b " + allFiles + " sonic.gpu.sp2");
+                createSP2.WorkingDirectory = currentPath;
+                createSP2.WindowStyle = ProcessWindowStyle.Hidden;
+                var begin = Process.Start(createSP2);
+                begin.WaitForExit();
+                begin.Close();
+
+                //ofd_OpenFiles.Title = "Please select the original SP2 file.";
+
+                //if (ofd_OpenFiles.ShowDialog() == DialogResult.OK)
+                //{
+                    
+                //}
+            }
+        }
+
+        void Main_About_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "SP2 Tool\n" +
+                "Developed by Hyper Development Team\n\n" + versionNumber +
+                "\n\nCredits:\n" +
+                "Hyper - Lead Developer\n" +
+                "Natsumi - Byte Parser\n" +
+                "Beatz - Researcher", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        static readonly string applicationData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+        void Main_Load(object sender, EventArgs e)
+        {
+            #region Directory Check...
+            if (!Directory.Exists(applicationData + @"\Hyper_Development_Team\SP2 Tool\")) Directory.CreateDirectory(applicationData + @"\Hyper_Development_Team\SP2 Tool\");
+            if (!Directory.Exists(applicationData + @"\Hyper_Development_Team\SP2 Tool\Archives\")) Directory.CreateDirectory(applicationData + @"\Hyper_Development_Team\SP2 Tool\Archives\");
+            #endregion
+
+            Text = "SP2 Tool (" + versionNumber + ")";
+        }
+
+        void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var archiveData = new DirectoryInfo(applicationData + @"\Hyper_Development_Team\SP2 Tool\Archives\" + sessionID);
+
+            try
+            {
+                if (Directory.Exists(applicationData + @"\Hyper_Development_Team\SP2 Tool\Archives\" + sessionID))
+                {
+                    foreach (FileInfo file in archiveData.GetFiles())
+                    {
+                        file.Delete();
+                    }
+                    foreach (DirectoryInfo directory in archiveData.GetDirectories())
+                    {
+                        directory.Delete(true);
+                    }
+                }
+            }
+            catch { }
+        }
+
+        void Main_GitHub_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/HyperPolygon64/SP2-Tool");
+        }
+    }
+}
